2e438bcda0f3c31b785e5bca37ea5c18e1d27bff
UpperClass5 <tim.stoecker5@web.de>
2025-05-05 14:39:11 +0200

    custamize the routes and make SendMessage to required

--- FHDW-CHAT-Backend/Controllers/ChatController.cs
+++ FHDW-CHAT-Backend/Controllers/ChatController.cs
@@ -1,27 +1,29 @@
 ﻿using FHDW_CHAT_Backend.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 
 namespace ChatBackend.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ChatController : ControllerBase
     {
         private static readonly string FilePath = "chatlog.json";
         private static List<ChatMessage> messages = LoadChatLog();
 
-        // GET api/chat
+        // GET Chat/getChatLog
         [HttpGet]
+        [Route("getChatLog")]
         public IActionResult GetChatLog()
         {
             return Ok(messages);
         }
 
-        // POST api/chat
+        // POST Chat/sendMessage
         [HttpPost]
-        public IActionResult SendMessage([FromBody] ChatMessage message)
+        [Route("sendMessage")]
+        public IActionResult SendMessage([Required, FromBody] ChatMessage message)
         {
             // Save the incoming message
             message.TimeStamp = DateTime.UtcNow.ToString("o"); //2025-05-02T19:18:33.2947218Z Nur nötig wenn Client keinen TimeStamp sendet //TODO: Entscheiden ob Client oder Serverseitiges Handeln der TimeStamps!
--- FHDW-CHAT-Backend/obj/Debug/net8.0/FHDW-CHAT-Backend.AssemblyInfo.cs
+++ FHDW-CHAT-Backend/obj/Debug/net8.0/FHDW-CHAT-Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FHDW-CHAT-Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11714462ffa8d3d986c529bc0f3e3464f202d68d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc13f96f949ab5c1139833aa98919b271099f25f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FHDW-CHAT-Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FHDW-CHAT-Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
