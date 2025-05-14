b1106c3e26b14e5fb483535273fd90991eca3b49
BXZ521 <126605299+BXZ521@users.noreply.github.com>
2025-05-14 13:34:28 +0200

    front end connection enabled

--- FHDW-CHAT-Backend/Program.cs
+++ FHDW-CHAT-Backend/Program.cs
@@ -7,7 +7,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:3000")
               .AllowAnyMethod()
               .AllowAnyHeader();
     });
--- FHDW-CHAT-Backend/obj/Release/net8.0/FHDW-CHAT-Backend.AssemblyInfo.cs
+++ FHDW-CHAT-Backend/obj/Release/net8.0/FHDW-CHAT-Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FHDW-CHAT-Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+333bf2a6e0eb59a21436b41443c7f6b1dde8f7d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea67980e7eb06a14841915ef517396242be99fc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("FHDW-CHAT-Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FHDW-CHAT-Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
